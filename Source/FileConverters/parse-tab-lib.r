d88e65c5ce0394c967ec5ffea882a7049c1593e4
Luke Emmet <luke.emmet@gmail.com>
2019-10-12 23:21:50 +0100

    update to use TAB 4.3.98 improve TAB parser to allow comma as ornament build as 1.1.7

--- Source/FileConverters/parse-tab-lib.r
+++ Source/FileConverters/parse-tab-lib.r
@@ -131,7 +131,7 @@ parse-tab: func [input-string] [
         ;---etc
     ]
     
-    ornaments: charset join ".:|-+*#x$<'`Q%?>@=_wuU(){}[]" {"}
+    ornaments: charset join ".,:|-+*#x$<'`Q%?>@=_wuU(){}[]" {"}
     insert ornaments #"^^"    ;---trying to insert "^" into ornaments bitset...
     insert ornaments #"]"
     insert ornaments #"."
